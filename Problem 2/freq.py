ec6afddfb401c38bab4c55c01de1aa4b5caede1f
chairielazizi <azizihr2000@gmail.com>
2021-05-20 19:55:07 +0800

    update problem 2

--- Problem 2/freq.py
+++ Problem 2/freq.py
@@ -1,9 +1,8 @@
 import urllib.request, urllib.error, urllib.parse, obo
 import plotly, plotly.graph_objects as go
 
+# article's URL
 url = 'https://www.oldbaileyonline.org/browse.jsp?id=t17800628-33&div=t17800628-33'
-# url = 'https://www.textise.net/showText.aspx?strURL=https://www.thestar.com.my/business/business-news/2015/01/05/citylink-mulls-main-market-listing-in-three-years#textiseTop'
-
 
 # words without stop words
 response = urllib.request.urlopen(url)
@@ -14,17 +13,32 @@ wordlist = obo.removeStopwords(fullwordlist, obo.stopwords)
 dictionary = obo.wordListToFreqDict(wordlist)
 sorteddict = obo.sortFreqDict(dictionary)
 
+# display word and frequency
 for s in sorteddict: print(str(s))
+
+positivewordlist = obo.inPositive(wordlist, obo.PositiveWord)
+negativewordlist = obo.inNegative(wordlist, obo.NegativeWord)
 
 # plot histogram of word frequency
 for s in wordlist:
     x = [a for a in wordlist]
     y = [wordlist.count(p) for p in wordlist]
-
 fig = go.Figure()
 fig.add_trace(go.Histogram(histfunc="count", y=y, x=x, name="count"))
-
 fig.show()
 
+# plot histogram of positive word frequency
+for s in positivewordlist:
+    x = [a for a in positivewordlist]
+    y = [positivewordlist.count(p) for p in positivewordlist]
+fig = go.Figure()
+fig.add_trace(go.Histogram(histfunc="count", y=y, x=x, name="count"))
+fig.show()
 
-
+# plot histogram of negative word frequency
+for s in negativewordlist:
+    x = [a for a in negativewordlist]
+    y = [negativewordlist.count(p) for p in negativewordlist]
+fig = go.Figure()
+fig.add_trace(go.Histogram(histfunc="count", y=y, x=x, name="count"))
+fig.show()
--- Problem 2/obo.py
+++ Problem 2/obo.py
@@ -1,58 +1,20 @@
-# obo.py
+# stop words list
+st = open("stopwords.txt")
+stop = st.read()
+stopwords = stop.split(",")
 
-stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards']
-stopwords += ['again', 'against', 'all', 'almost', 'alone', 'along']
-stopwords += ['already', 'also', 'although', 'always', 'am', 'among']
-stopwords += ['amongst', 'amoungst', 'amount', 'an', 'and', 'another']
-stopwords += ['any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere']
-stopwords += ['are', 'around', 'as', 'at', 'back', 'be', 'became']
-stopwords += ['because', 'become', 'becomes', 'becoming', 'been']
-stopwords += ['before', 'beforehand', 'behind', 'being', 'below']
-stopwords += ['beside', 'besides', 'between', 'beyond', 'bill', 'both']
-stopwords += ['bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant']
-stopwords += ['co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de']
-stopwords += ['describe', 'detail', 'did', 'do', 'done', 'down', 'due']
-stopwords += ['during', 'each', 'eg', 'eight', 'either', 'eleven', 'else']
-stopwords += ['elsewhere', 'empty', 'enough', 'etc', 'even', 'ever']
-stopwords += ['every', 'everyone', 'everything', 'everywhere', 'except']
-stopwords += ['few', 'fifteen', 'fifty', 'fill', 'find', 'fire', 'first']
-stopwords += ['five', 'for', 'former', 'formerly', 'forty', 'found']
-stopwords += ['four', 'from', 'front', 'full', 'further', 'get', 'give']
-stopwords += ['go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her']
-stopwords += ['here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers']
-stopwords += ['herself', 'him', 'himself', 'his', 'how', 'however']
-stopwords += ['hundred', 'i', 'ie', 'if', 'in', 'inc', 'indeed']
-stopwords += ['interest', 'into', 'is', 'it', 'its', 'itself', 'keep']
-stopwords += ['last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made']
-stopwords += ['many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine']
-stopwords += ['more', 'moreover', 'most', 'mostly', 'move', 'much']
-stopwords += ['must', 'my', 'myself', 'name', 'namely', 'neither', 'never']
-stopwords += ['nevertheless', 'next', 'nine', 'no', 'nobody', 'none']
-stopwords += ['noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of']
-stopwords += ['off', 'often', 'on', 'once', 'one', 'only', 'onto', 'or']
-stopwords += ['other', 'others', 'otherwise', 'our', 'ours', 'ourselves']
-stopwords += ['out', 'over', 'own', 'part', 'per', 'perhaps', 'please']
-stopwords += ['put', 'rather', 're', 's', 'same', 'see', 'seem', 'seemed']
-stopwords += ['seeming', 'seems', 'serious', 'several', 'she', 'should']
-stopwords += ['show', 'side', 'since', 'sincere', 'six', 'sixty', 'so']
-stopwords += ['some', 'somehow', 'someone', 'something', 'sometime']
-stopwords += ['sometimes', 'somewhere', 'still', 'such', 'system', 'take']
-stopwords += ['ten', 'than', 'that', 'the', 'their', 'them', 'themselves']
-stopwords += ['then', 'thence', 'there', 'thereafter', 'thereby']
-stopwords += ['therefore', 'therein', 'thereupon', 'these', 'they']
-stopwords += ['thick', 'thin', 'third', 'this', 'those', 'though', 'three']
-stopwords += ['three', 'through', 'throughout', 'thru', 'thus', 'to']
-stopwords += ['together', 'too', 'top', 'toward', 'towards', 'twelve']
-stopwords += ['twenty', 'two', 'un', 'under', 'until', 'up', 'upon']
-stopwords += ['us', 'very', 'via', 'was', 'we', 'well', 'were', 'what']
-stopwords += ['whatever', 'when', 'whence', 'whenever', 'where']
-stopwords += ['whereafter', 'whereas', 'whereby', 'wherein', 'whereupon']
-stopwords += ['wherever', 'whether', 'which', 'while', 'whither', 'who']
-stopwords += ['whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with']
-stopwords += ['within', 'without', 'would', 'yet', 'you', 'your']
-stopwords += ['yours', 'yourself', 'yourselves']
+# positive words list
+f = open("Positive.txt")
+positive = f.read()
+PositiveWord = positive.split(",")
+
+# negative words list
+g = open("Negative.txt")
+negative = g.read()
+NegativeWord = negative.split(",")
 
 
+# turn html to text
 def stripTags(pageContents):
     pageContents = str(pageContents)
     startLoc = pageContents.find("<p>")
@@ -75,9 +37,8 @@ def stripTags(pageContents):
 
     return text
 
+    # remove all non-alphanumeric characters (using Unicode definition of alphanumeric).
 
-# Given a text string, remove all non-alphanumeric
-# characters (using Unicode definition of alphanumeric).
 
 def stripNonAlphaNum(text):
     import re
@@ -95,15 +56,20 @@ def sortFreqDict(freqdict):
     aux.reverse()
     return aux
 
+    # remove stop words
 
-# Given a list of words, remove any that are
-# in a list of stop words.
 
 def removeStopwords(wordlist, stopwords):
     return [w for w in wordlist if w not in stopwords]
 
+    # return positive words
 
 
+def inPositive(wordlist, PositiveWord):
+    return [p for p in wordlist if p in PositiveWord]
+
+    # return negative words
 
 
-
+def inNegative(wordlist, NegativeWord):
+    return [n for n in wordlist if n in NegativeWord]
