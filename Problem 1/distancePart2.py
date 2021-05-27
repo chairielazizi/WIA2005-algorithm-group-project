c5f4e5b045e1cf3416b987659be185f3f8fe17ae
syafiqah-violet <nursyafiqah.sarifishak@gmail.com>
2021-05-27 22:43:12 +0800

    update

--- Problem 1/distancePart2.py
+++ Problem 1/distancePart2.py
@@ -1,6 +1,34 @@
 import googlemaps
 
+import time
+start_time = time.time()
+
 gmaps = googlemaps.Client(key='AIzaSyDBQehfRCytJvvYHu4pelPuRw49m9gzYoc')
+
+
+def partition(array, low, high):
+    
+    pivot = array[high]   # choose the rightmost element as pivot
+    i = low - 1           # pointer for greater element
+    
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+    return i + 1
+
+
+def quickSort(array, low, high):
+    if low < high:
+ 
+        pi = partition(array, low, high)
+
+        quickSort(array, low, pi - 1)
+
+        quickSort(array, pi + 1, high)
 
 
 oriCus1 = (3.3615395462207878, 101.56318183511695)
@@ -18,7 +46,9 @@ courierCompany = [(3.0319924887507144, 101.37344116244806), #City-link Express
                   (3.2127230893650065, 101.57467295692778)]  #DHL
                   
 
-total_list=[]
+total_list1=[]
+total_list2=[]
+total_list3=[]
 for z in courierCompany:
     d1 = gmaps.distance_matrix( oriCus1, z , mode='driving')['rows'][0]['elements'][0]["distance"]["value"] 
     d2 = gmaps.distance_matrix( z , desCus1, mode='driving')['rows'][0]['elements'][0]["distance"]["value"] 
@@ -26,9 +56,12 @@ for z in courierCompany:
     result2 = d2/1000
 
     total = result1 + result2
-    total_list.append(total)
+    total_list1.append(total)
     print("Delivery Hubs",z,"=",total,"km")
-print("The shortest distance parcel travel from Rawang to Bukit Jelutong is",min(total_list),"km\n\n")
+quickSort(total_list1, 0, len(total_list1)-1)
+print(total_list1,"\n")
+
+print("The shortest distance parcel travel from Rawang to Bukit Jelutong is",total_list1[0],"km\n\n")
 
 for z in courierCompany:
     d1 = gmaps.distance_matrix( oriCus2, z , mode='driving')['rows'][0]['elements'][0]["distance"]["value"] 
@@ -37,9 +70,12 @@ for z in courierCompany:
     result2 = d2/1000
 
     total = result1 + result2
-    total_list.append(total)
+    total_list2.append(total)
     print("Delivery Hubs",z,"=",total,"km")
-print("The shortest distance parcel travel from Subang Jaya to Puncak Alam is",min(total_list),"km\n\n")
+quickSort(total_list2, 0, len(total_list2)-1)
+print(total_list2,"\n")
+
+print("The shortest distance parcel travel from Subang Jaya to Puncak Alam is",total_list2[0],"km\n\n")
 
 for z in courierCompany:
     d1 = gmaps.distance_matrix( oriCus3, z , mode='driving')['rows'][0]['elements'][0]["distance"]["value"] 
@@ -48,8 +84,12 @@ for z in courierCompany:
     result2 = d2/1000
 
     total = result1 + result2
-    total_list.append(total)
+    total_list3.append(total)
     print("Delivery Hubs",z,"=",total,"km")
-print("The shortest distance parcel travel from Ampang to Cyberjaya is",min(total_list),"km")
+quickSort(total_list3, 0, len(total_list3)-1)
+print(total_list3,"\n")
 
+print("The shortest distance parcel travel from Ampang to Cyberjaya is",total_list3[0],"km")
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
