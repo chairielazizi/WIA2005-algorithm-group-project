e6cafcfa7e7d870ade76309d738d97b01e3d935e
marsyaaa <marsyaamelia00@gmail.com>
2021-06-16 22:26:40 +0800

    integrated distance and plot, changed map outputs

--- Problem 1/distanceMerge.py
+++ Problem 1/distanceMerge.py
@@ -1,3 +1,4 @@
+from re import T
 import time
 start_time = time.time()
 import googlemaps
@@ -5,22 +6,22 @@ import googlemaps
 # to find the coordinate of the customers origin & destination
 def findLocation(index):
     if index == 1:
-        location = (3.3615395462207878, 101.56318183511695) # customer 1 origin
+        location = (3.3615395462207878,101.56318183511695) # customer 1 origin
 
     elif index == 2:
-        location = (3.049398375759954, 101.58546611160301) # customer 2 origin
+        location = (3.049398375759954,101.58546611160301) # customer 2 origin
 
     elif index == 3:
-        location = (3.141855957281073, 101.76158583424586)  # customer 3 origin
+        location = (3.141855957281073,101.76158583424586)  # customer 3 origin
 
     elif index == 4:
-        location = (3.1000170516638885, 101.53071480907951) # customer 1 destination
+        location = (3.1000170516638885,101.53071480907951) # customer 1 destination
 
     elif index == 5:
-        location = (3.227994355250716, 101.42730357605375)  # customer 2 destination
+        location = (3.227994355250716,101.42730357605375)  # customer 2 destination
 
     else:
-        location = (2.9188704151716256, 101.65251821655471) # customer 3 destination
+        location = (2.9188704151716256,101.65251821655471) # customer 3 destination
 
     return location
 
@@ -94,15 +95,26 @@ def quickSort(array, low, high):
         quickSort(array, pi + 1, high)
 
 
-def printTotalDistance(list):
+def printTotalDistance(total_list):
+    
     courierName = ["City-Link", "Pos Laju", "GDEX", "J&T", "DHL"]
 
     for j in range(len(courierName)):
-        print(courierName[j], ": ", list[j], " km")
+        print(courierName[j], ": ", total_list[j], " km")
 
     print()
 
- 
+
+def courierSorting(total_list):
+    
+    courierName = ["City-Link", "Pos Laju", "GDEX", "J&T", "DHL"]
+
+    total_list_zip = list(zip(*[total_list, courierName]))
+    quickSort(total_list_zip, 0, len(total_list_zip)-1)
+
+    return total_list_zip
+
+
 if __name__ == '__main__':
     #display the distance  
     print("The distance between Rawang and Bukit Jelutong is",findDistance(1), "km")
@@ -114,18 +126,18 @@ if __name__ == '__main__':
 
     #display all distances and insert quicksort to sort the list   
     printTotalDistance(total1_list)
-    quickSort(total1_list, 0, len(total1_list)-1)
+    total1_list = courierSorting(total1_list)
     print(total1_list)
-    print("The shortest distance parcel travel from Rawang to Bukit Jelutong is",total1_list[0],"km\n")
+    print("The shortest distance parcel travel from Rawang to Bukit Jelutong is ",total1_list[0][0],"km\n")
 
     printTotalDistance(total2_list)
-    quickSort(total2_list, 0, len(total2_list)-1)
+    total2_list = courierSorting(total2_list)
     print(total2_list)
-    print("The shortest distance parcel travel from Subang Jaya to Puncak Alam is",total2_list[0],"km\n")
+    print("The shortest distance parcel travel from Subang Jaya to Puncak Alam is ",total2_list[0][0],"km\n")
 
     printTotalDistance(total3_list)
-    quickSort(total3_list, 0, len(total3_list)-1)
+    total3_list = courierSorting(total3_list)
     print(total3_list)
-    print("The shortest distance parcel travel from Ampang to Cyberjaya is",total3_list[0],"km\n")
+    print("The shortest distance parcel travel from Ampang to Cyberjaya is ",total3_list[0][0],"km\n")
 
     print("--- %s seconds ---" % (time.time() - start_time))
--- Problem 1/plot.py
+++ Problem 1/plot.py
@@ -1,76 +1,105 @@
 import gmplot
 import requests
 import polyline
+from distanceMerge import findLocation, findTotalDistance, courierSorting
+
+# to find the coordinate of the delivery hubs
+def findHub(index):
+    if index == 1 or index == "City-Link":
+        location = (3.0319924887507144,101.37344116244806) # citylink
+
+    elif index == 2 or index == 'Pos Laju':
+        location = (3.112924170027219,101.63982650389863) # poslaju
+
+    elif index == 3 or index == 'GDEX':
+        location = (3.265154613796736,101.68024844550233)  # gdex
+
+    elif index == 4 or index =='J&T':
+        location = (2.9441205329488325,101.7901521759029) # j&t
+
+    elif index == 5 or index == "DHL":
+        location = (3.2127230893650065,101.57467295692778)  # dhl
+
+    return location
+
 
 # scatter the markers
 def scatter():
-        latMark, lngMark = zip(*[(3.3615395462207878, 101.56318183511695), (3.1000170516638885, 101.53071480907951),    # customer 1
-                                (3.049398375759954, 101.58546611160301), (3.227994355250716, 101.42730357605375),       # customer 2
-                                (3.141855957281073, 101.76158583424586), (2.9188704151716256, 101.65251821655471),      # customer 3
-                                (3.0319924887507144, 101.37344116244806), (3.112924170027219, 101.63982650389863), (3.265154613796736, 101.68024844550233),     #citylink, poslaju, gdex
-                                (2.9441205329488325, 101.7901521759029), (3.2127230893650065, 101.57467295692778)])     #j&t, dhl
+	latMark, lngMark = zip(*[
+		findLocation(1), findLocation(2), findLocation(3),				# origins
+		findLocation(4), findLocation(5), findLocation(6),				# destinations
+		findHub(1), findHub(2), findHub(3), findHub(4), findHub(5)])	# couriers
 
-        title = ["Customer 1 Origin", "Customer 1 Destination", "Customer 2 Origin", "Customer 2 Destination", "Customer 3 Origin", "Customer 3 Destination",
-                "City-Link Express", "Poslaju", "GDEX", "J&T", "DHL"]
+	title = ["Customer 1 Origin", "Customer 2 Origin", "Customer 3 Origin", "Customer 1 Destination", "Customer 2 Destination",
+			"Customer 3 Destination", "City-Link Express", "Poslaju", "GDEX", "J&T", "DHL"]
 
-        label = ["1", "1", "2", "2", "3", "3", "C", "P", "G", "J", "D"]
+	label = ["1", "2", "3", "1", "2", "3", "C", "P", "G", "J", "D"]
 
-        gmap.scatter(latMark, lngMark, color="#FF0000", size=50, title=title, label=label)
+	gmap.scatter(latMark, lngMark, color="#FF0000", size=50, title=title, label=label)
+	gmap.draw("Problem 1/mapInitial.html")
+
+
+# choose map to draw
+def draw(num):
+	if num == 0:
+		gmap.draw("Problem 1/map1.html")   
+
+	elif num == 1:
+		gmap.draw("Problem 1/map2.html")   
+
+	else:
+		gmap.draw("Problem 1/map3.html") 
 
 
 # plot all 5 routes for each customer
 def plot():
-        waypoints = ["3.0319924887507144,101.37344116244806", "3.112924170027219,101.63982650389863", "3.265154613796736,101.68024844550233", 
-                        "2.9441205329488325,101.7901521759029", "3.2127230893650065,101.57467295692778"]
+	colours = ["#008000", "#808000", "#2E8B57", "#90EE90", "#00FF00",   # customer 1
+				"#00008B", "#0000CD", "#6495ED", "#87CEFA", "#1E90FF",  # customer 2
+				"#C71585", "#FF69B4", "#DB7093", "#FF1493", "#FFB6C1"]  # customer 3
 
-        colours = ["#008000", "#808000", "#2E8B57", "#90EE90", "#00FF00",   # customer 1
-                "#00008B", "#0000CD", "#6495ED", "#87CEFA", "#1E90FF",  # customer 2
-                "#C71585", "#FF69B4", "#DB7093", "#FF1493", "#FFB6C1"]  # customer 3
+	k = 0   # initiate colours
 
-        k = 0   # initiate colours
+	for i in range(3):
 
-        for i in range(len(origin)):
+		for j in range(5):
 
-                for j in range(len(waypoints)):
+			url = ("https://maps.googleapis.com/maps/api/directions/json?&origin=\"{}\"&destination=\"{}\"&waypoints=\"{}\"&key={}"
+					).format(findLocation(i+1), findLocation(i+4), findHub(j+1), apikey)
 
-                        url = ("https://maps.googleapis.com/maps/api/directions/json?&origin={}&destination={}&waypoints={}&key={}"
-                                ).format(origin[i], destination[i], waypoints[j], apikey)
+			r = requests.get(url) 
+			latPlot, lngPlot = zip(*polyline.decode(r.json()["routes"][0]["overview_polyline"]["points"]))
+			gmap.plot(latPlot, lngPlot, colours[k], edge_width=7.5)         # plot routes with colours
+			k = k + 1       # change colours
 
-                        r = requests.get(url) 
-                        latPlot, lngPlot = zip(*polyline.decode(r.json()["routes"][0]["overview_polyline"]["points"]))
-                        gmap.plot(latPlot, lngPlot, colours[k], edge_width=7.5)         # plot routes with colours
-                        k = k + 1       # change colours
-
-        gmap.draw("Problem 1/map.html")   # whole map after plotting
+		draw(i)
 
 
 # plot shortest route for each customer
 def plotShortest():
-        waypoints = ["3.2127230893650065,101.57467295692778", "3.2127230893650065,101.57467295692778", "2.9441205329488325,101.7901521759029"]
+	
+	total1_list, total2_list, total3_list = findTotalDistance(1), findTotalDistance(2), findTotalDistance(3)
+	total1_list, total2_list, total3_list = courierSorting(total1_list), courierSorting(total2_list), courierSorting(total3_list)
+	waypoints = [findHub(total1_list[0][1]), findHub(total2_list[0][1]), findHub(total3_list[0][1])]
 
-        colours = ["#00FF00", "#1E90FF", "#FF1493"]
+	colours = ["#00FF00", "#1E90FF", "#FF1493"]
 
-        for i in range(len(origin)):
+	for i in range(3):
 
-                url = ("https://maps.googleapis.com/maps/api/directions/json?&origin={}&destination={}&waypoints={}&key={}"
-                        ).format(origin[i], destination[i], waypoints[i], apikey)
+		url = ("https://maps.googleapis.com/maps/api/directions/json?&origin=\"{}\"&destination=\"{}\"&waypoints=\"{}\"&key={}"
+				).format(findLocation(i+1), findLocation(i+4), waypoints[i], apikey)
 
-                r = requests.get(url) 
-                latPlot, lngPlot = zip(*polyline.decode(r.json()["routes"][0]["overview_polyline"]["points"]))
-                gmap.plot(latPlot, lngPlot, colours[i], edge_width=7.5)         # plot shortest route with colour
+		r = requests.get(url) 
+		latPlot, lngPlot = zip(*polyline.decode(r.json()["routes"][0]["overview_polyline"]["points"]))
+		gmap.plot(latPlot, lngPlot, colours[i], edge_width=7.5)         # plot shortest route with colour
 
-        gmap.draw("Problem 1/mapShortest.html")   # whole map after plotting
+	gmap.draw("Problem 1/mapShortest.html") 
 
 
 if __name__ == "__main__":
-        apikey = "AIzaSyDBQehfRCytJvvYHu4pelPuRw49m9gzYoc"
+	apikey = "AIzaSyDBQehfRCytJvvYHu4pelPuRw49m9gzYoc"
 
-        gmap = gmplot.GoogleMapPlotter(3.104637, 101.581797, zoom=10, apikey=apikey)    # whole map before plotting
+	gmap = gmplot.GoogleMapPlotter(3.104637, 101.581797, zoom=10, apikey=apikey)    # whole map before plotting
 
-        origin = ["3.3615395462207878,101.56318183511695", "3.049398375759954,101.58546611160301", "3.141855957281073,101.76158583424586"]
-
-        destination = ["3.1000170516638885,101.53071480907951", "3.227994355250716,101.42730357605375", "2.9188704151716256,101.65251821655471"]
-
-        scatter()
-        plot()
-        plotShortest()
+	scatter()
+	plot()
+	plotShortest()
